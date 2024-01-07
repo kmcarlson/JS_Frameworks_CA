86b6ca503867ee04c4d040364256ba666c9fd497
kmcarlson <77496513+kmcarlson@users.noreply.github.com>
2024-01-07 20:34:47 +0100

    fix cart and CheckoutSuccess and format

--- jsframeworksca/src/App.js
+++ jsframeworksca/src/App.js
@@ -1,38 +1,46 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Layout from './comp/Layout/layout'; 
-import Cart from './pages/cart';
-import Home from './pages/home';
-import Contact from './pages/contact';
-import ProductDetails from './pages/productDetails';
-import CheckoutSuccess from './pages/checkoutSuccess';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "./comp/Layout/layout";
+import Cart from "./pages/cart";
+import Home from "./pages/home";
+import Contact from "./pages/contact";
+import ProductDetails from "./pages/productDetails";
+import CheckoutSuccess from "./pages/checkoutSuccess";
+import "./App.css";
 
 function App() {
- const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
- const addToCart = (product) => {
- setCart([...cart, product]);
- };
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
 
- const removeFromCart = (id) => {
- setCart(cart.filter((product) => product.id !== id));
- };
+  const removeFromCart = (id) => {
+    setCart(cart.filter((product) => product.id !== id));
+  };
 
- return (
- <Router>
-   <Layout>
-     <Routes>
-       <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} />} />
-       <Route path="/contact" element={<Contact />} />
-       <Route path="/" element={<Home />} /> 
-       <Route path="/product/:id" element={<ProductDetails addToCart={addToCart} />} />
-       {/* <Route path="/checkoutSuccess" element={<CheckoutSuccess />} /> */}
-       <Route path="/checkoutSuccess" element={<CheckoutSuccess setCart={setCart} />} />
-     </Routes>
-   </Layout>
- </Router>
- );
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route
+            path="/cart"
+            element={<Cart cart={cart} removeFromCart={removeFromCart} />}
+          />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/product/:id"
+            element={<ProductDetails addToCart={addToCart} />}
+          />
+          <Route
+            path="/checkoutSuccess"
+            element={<CheckoutSuccess setCart={setCart} />}
+          />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
 export default App;
--- jsframeworksca/src/pages/checkoutSuccess.js
+++ jsframeworksca/src/pages/checkoutSuccess.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const CheckoutSuccess = () => {
-    
-    return (
-        <div>
-            <h1>Takk for din bestilling!</h1>
-            <p>Vi setter pris på din støtte.</p>
-            <p>Din bestilling vil bli behandlet og sendt så snart som mulig.</p>
-            <p>Ta gjerne kontakt med oss hvis du har spørsmål.</p>
-        </div>
-    );
-};
+function CheckoutSuccess({ setCart }) {
+  React.useEffect(() => {
+    setCart([]);
+  }, [setCart]);
+
+  return (
+    <div>
+      <h1>Takk for din bestilling!</h1>
+      <p>Vi setter pris på din støtte.</p>
+      <p>Din bestilling vil bli behandlet og sendt så snart som mulig.</p>
+      <p>Ta gjerne kontakt med oss hvis du har spørsmål.</p>
+    </div>
+  );
+}
 
 export default CheckoutSuccess;
-
